1cfd77318b92ee07382c46725c2e832a784ab2ef
Mike Hansford <mjhansford@gmail.com>
2025-01-08 22:08:57 +1100

    Separating into many playbooks

---
+++ files/profile/my-bashrc.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+alias cd..='cd ..'
+alias nvim='~/bin/nvim-linux64/bin/nvim'
+
+# export PATH=$PATH:~/bin
+
+#color_prompt=yes
+
+#if [ "$color_prompt" = yes ]; then
+#    PS1='\n${debian_chroot:+($debian_chroot)}\[\033[01;35m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\n[\!] \$ '
+#      PS1='\n${debian_chroot:+($debian_chroot)}\u@\h \$'
+#else
+#     PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+#fi
